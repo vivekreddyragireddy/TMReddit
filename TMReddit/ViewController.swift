5416365c23073aebf9b8dc844b06a6e8176cf0c8
vivek <vivek.ragireddy2@t-mobile.com>
2018-01-10 09:36:05 -0800

    Initial Commit

---
+++ TMReddit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TMReddit
+//
+//  Created by vivek on 1/10/18.
+//  Copyright © 2018 vivek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
